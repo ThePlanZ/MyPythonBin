1eb47e9968ab4a1e259323a5e1491a2ccbc1bc4b
The Plan Z <134126619+ThePlanZ@users.noreply.github.com>
2026-07-09 12:54:23 +0200

    fixed rate not working because im dumb and stupid and never check decimals

--- tinyinterestcalculator.py
+++ tinyinterestcalculator.py
@@ -3,8 +3,8 @@ p=["Initial Principal Balance", "Rate (In Decimal)", "Number of Years Elapsed"]
 for i in range(len(v)):
     t=""
     i=int(i)
-    while not t.isdigit() or int(t) < 0:
+    while not t.replace(".","",1).isdigit() or float(t) <= 0:
         t=input(f"{p[i]} : ")
     v[i]=float(t)
-t = v[0]*pow(1+v[1],v[2])
-print(t)
+f = v[0]*pow(1+v[1],v[2])
+print(f)
