cb647037fa9c10198108a1e9fb0328375051dcf6
The Plan Z <134126619+ThePlanZ@users.noreply.github.com>
2026-07-09 12:41:54 +0200

    Update tinyinterestcalculator.py

--- tinyinterestcalculator.py
+++ tinyinterestcalculator.py
@@ -3,7 +3,7 @@ p=["Initial Principal Balance", "Rate (In Decimal)", "Number of Years Elapsed"]
 for i in range(len(v)):
     t="0"
     i=int(i)
-    while not t.isdigit() or int(t) <= 0:
+    while not t.isdigit() or int(t) < 0:
         t=input(f"{p[i]} : ")
     v[i]=float(t)
 t = v[0]*pow(1+v[1],v[2])
