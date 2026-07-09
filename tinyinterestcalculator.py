073337b7600313930e48f951768815a62ba44b53
The Plan Z <134126619+ThePlanZ@users.noreply.github.com>
2026-07-09 12:24:18 +0200

    Create tinyinterestcalculator.py

---
+++ tinyinterestcalculator.py
@@ -0,0 +1,10 @@
+v = ["", "", "",]
+varprompt = ["Initial Principal Balance", "Rate (Decimal)", "Number of Years Elapsed",]
+for i in range(len(v)):
+    temp = "0"
+    i = int(i)
+    while not temp.isalnum() or temp.upper() != temp.lower() or int(temp) <= 0:
+        temp = input(f"{varprompt[i]} : ")
+    v[i] = float(temp)
+total = v[0] * pow(1 + v[1], v[2])
+print(total)
